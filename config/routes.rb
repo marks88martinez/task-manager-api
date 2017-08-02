6f3c1a4192077d3653bba82c9f0373bcc7465ae6
mark <marks88martinez@gmail.com>
2017-08-02 10:51:44 -0400

    api config

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
+require 'api_version_constraint'
 Rails.application.routes.draw do
 
   namespace :api, default: { format: :json}, constraints: {subdomain: 'api'}, path: "/" do
+    namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
+      # resources :tasks
+    end
 
   end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
